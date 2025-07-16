4c8d4822471d3b08193cc1e80adf35c31fdd2f2e
Sween Khan <khansween@gmail.com>
2025-07-16 13:47:58 +0530

    first day of js

---
+++ js/index.js
@@ -0,0 +1,38 @@
+//variables in js
+
+
+//types of variable in js?
+//let, const, var
+
+// naming rules in js.
+//first letter should be in lowerace
+//if you are using two words for naming, then it should be in camelCase
+// you can also add number in variable name, 
+
+
+//-----------------------------------------------------------------------------------------------//
+//A. let variable:    you can reAssign the value in let variable
+let studentName = "roshan" 
+let student2 = "rahul" 
+let student3 = "bhanwar"
+
+student3 = "bhanwar lal"
+
+
+
+console.log(studentName, student2, student3)
+ 
+
+//B. const vairable:     you should create const variable for constant values
+const dob = "3jun 2002"
+
+
+
+//C. var variable:    you should'nt use this variable. 
+var mobile = 588588585588
+
+var mobile = 958958588585
+
+ 
+
+
---
+++ js/introduction.js
@@ -0,0 +1,3 @@
+// what is js?
+// js is a programming language used to create a dynamic and inter-active elements on websites mobile app,
+// and even in some server side  application.
